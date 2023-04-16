974b88b3f9c372345d3700ac323797f2da3371c7
Patrick Buchholz <patrickbuchholz@appsfactory.de>
2023-04-16 11:49:56 +0200

    [ADD] created realm and drift projects

---
+++ drift_cars/android/app/src/main/kotlin/de/patrickbtud/drift_cars/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.patrickbtud.drift_cars
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ realm_cars/android/app/src/main/kotlin/de/patrickbtud/realm_cars/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.patrickbtud.realm_cars
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
